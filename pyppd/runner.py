9c398cddec7f3cbc9dea604553e2067356af45a4
Vitor Baptista <vitor@lavid.ufpb.br>
2010-06-20 22:57:32 -0300

    Adds command-line argument parsing

--- pyppd/runner.py
+++ pyppd/runner.py
@@ -1,7 +1,25 @@
-import sys
+from optparse import OptionParser
+from os.path import isdir
 import compressor
 
 def run():
-    output = open("ppds.pkl", "wb")
-    output.write(compressor.compress(sys.argv[1]))
+    parser = OptionParser(usage="usage: %prog [options] ppds_directory",
+                          version="%prog 0.1.0\n"
+                                  "Copyright (C) 2010 Vitor Baptista.\n"
+                                  "This is free software; see the source for copying conditions.\n"
+                                  "There is NO warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.")
+    parser.add_option("-o", "--output",
+                      default="ppds.pkl", metavar="FILE",
+                      help="write archive to FILE [default %default]")
+    (options, args) = parser.parse_args()
+
+    if len(args) != 1:
+        parser.error("incorrect number of arguments")
+
+    ppds_directory = args[0]
+    if not isdir(ppds_directory):
+        parser.error(ppds_directory + " isn't a directory")
+
+    output = open(options.output, "wb")
+    output.write(compressor.compress(ppds_directory))
     output.close()
