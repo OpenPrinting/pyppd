e707e070e12a0451fa3854b2d1d8bec507bf5c97
Vítor Baptista <vitor@lavid.ufpb.br>
2010-08-01 19:15:48 -0300

    Adding a description and bumping version to 0.1.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='pyppd',
-    version='0.1.4',
+    version='0.1.5',
     author='Vitor Baptista',
     author_email='vitor@vitorbaptista.com',
     packages=['pyppd'],
@@ -10,5 +10,6 @@ setup(
     scripts=['bin/pyppd'],
     url='http://gitorious.org/vitorbaptista/pyppd/',
     license='GPLv3',
+    description='A CUPS PostScript Printer Driver\'s compressor and generator',
     long_description=open('README.txt').read(),
 )
