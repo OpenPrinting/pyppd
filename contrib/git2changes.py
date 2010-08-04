81586209eb83a3fb5336c5d0311977602baec6d0
Vítor Baptista <vitor@lavid.ufpb.br>
2010-08-04 04:44:02 -0300

    Generate CHANGES.txt from git log and add it to sdist.
    I made git2changes which simply gets the commit messages and organizes
    them into something similar to the GNU ChangeLog style. But, instead of
    separating the commits by day, I organize them by version. I think this
    way, even as it's not standard, generates a file that is more useful to
    pyppd's users.

---
+++ contrib/git2changes.py
@@ -0,0 +1,103 @@
+#!/usr/bin/python
+# Copyright 2010 Vitor Baptista <vitor@vitorbaptista.com>
+# Distributed under the terms of the GNU Lesser General Public License v3 or later
+
+from subprocess import Popen, PIPE
+import re
+
+class Commit:
+    def __init__(self, log):
+        log.strip()
+        log_array = log.split('\n')
+        self.commit = log_array.pop(0).split('commit ')[0]
+        self.author = log_array.pop(0).split('Author: ')[1]
+        self.date = log_array.pop(0).split('Date:   ')[1]
+
+        # Remove garbage from array
+        while True:
+            garbage = log_array.pop()
+            if garbage.find('files changed') != -1: break
+
+        self.changed_files = []
+        changed_file = log_array.pop()
+        while changed_file != "":
+            self.changed_files += [changed_file.split('|')[0].strip()]
+            changed_file = log_array.pop()
+
+        self.message = ""
+        try:
+            while True:
+                commit_line = log_array.pop().strip()
+                if (commit_line == "" or 
+                    commit_line.find('Signed-off-by') != -1 or
+                    commit_line.find('git-svn-id') != -1):
+                    commit_line = log_array.pop()
+                    continue
+                self.message = commit_line.strip() + " " + self.message
+        except IndexError:
+            pass
+
+        self.message = self.__break_string(self.message, 80)
+
+        self.version = None
+
+    def __break_string(self, string, column):
+        finished_string = ""
+        string = string.replace('\n', ' ')
+        while column < len(string):
+            break_point = string[0:column].rfind(' ')
+            if break_point == -1:
+                break_point = string[column + 1:].find(' ')
+                if break_point != -1:
+                    break_point += column
+
+            finished_string += string[0:break_point] + "\n"
+            string = string[break_point + 1:]
+
+        if string != "": finished_string += string
+
+        return finished_string
+
+
+    def __str__(self):
+        message_with_files = '* ' + ', '.join(self.changed_files) + ': ' + self.message
+        message_with_files = self.__break_string(message_with_files, 78)
+        message_with_files = '  ' + message_with_files.replace('\n', '\n  ')
+        return message_with_files
+
+
+def get_version(commit):
+    setup_py = Popen(["git", "show", "%s:setup.py" % commit], stdout=PIPE).communicate()[0]
+    version = setup_py.split("version='")[1].split("'")[0]
+    return version
+
+def run(output):
+    log = Popen(["git", "log", "--summary", "--stat", "--no-merges", "--date=short"], stdout=PIPE).communicate()[0]
+    log = "\n%s" % log
+    log_array = re.split('\ncommit ', log)
+    log_array.pop(0)
+    commits = []
+    for commit in log_array:
+        commits.insert(0, Commit(commit))
+
+    prevVersion = ""
+    for commit in commits:
+        version = get_version(commit.commit)
+        if prevVersion == "" or version != prevVersion:
+            commit.version = version
+        prevVersion = version
+
+    commits.reverse()
+    if not commits[0].version: commits[0].version = "HEAD"
+    for commit in commits:
+        if commit.version:
+            output_format = "v%s (%s)\n\n"
+            if commit.version == "HEAD": output_format = "%s (%s)\n\n"
+            output.write(output_format % (commit.version, commit.date))
+        output.write("%s\n\n" % commit)
+
+
+if __name__ == "__main__":
+    from sys import stdout
+    run(stdout)
+    stdout.close()
--- setup.py
+++ setup.py
@@ -1,6 +1,21 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from distutils.command.sdist import sdist as _sdist
+
+class sdist(_sdist):
+    def run(self):
+        try:
+            import sys
+            sys.path.append("contrib")
+            import git2changes
+            print 'generating CHANGES.txt'
+            with open('CHANGES.txt', 'w+') as f:
+                git2changes.run(f)
+        except ImportError:
+            pass
+        
+        _sdist.run(self)
 
 setup(
     name='pyppd',
@@ -14,4 +29,5 @@ setup(
     license='GPLv3',
     description='A CUPS PostScript Printer Driver\'s compressor and generator',
     long_description=open('README.txt').read(),
+    cmdclass={'sdist': sdist},
 )
