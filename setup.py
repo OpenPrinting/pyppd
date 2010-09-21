2f2e7d2f43e5d8fcaad42817257cbf61c067daf5
Vítor Baptista <vitor@vitorbaptista.com>
2010-09-21 19:21:19 -0300

    Changing version to 0.4.9

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ class sdist(_sdist):
 
 setup(
     name='pyppd',
-    version='0.4.8',
+    version='0.4.9',
     author='Vitor Baptista',
     author_email='vitor@vitorbaptista.com',
     packages=['pyppd'],
