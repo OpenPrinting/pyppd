c0ad5597fe6abd15ed3e29ef51ea6124e863f6ff
Till Kamppeter <till.kamppeter@gmail.com>
2025-02-19 17:14:56 +0100

    pyppd 1.1.1 Release

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ class sdist(_sdist):
 
 setup(
     name='pyppd',
-    version='1.1.0',
+    version='1.1.1',
     author='Vitor Baptista',
     author_email='vitor@vitorbaptista.com',
     packages=['pyppd'],
