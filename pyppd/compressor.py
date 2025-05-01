d482e28813041c5e8d4c97192f7ccd0c1ee45ce1
Rishav Tarway <rishavtarway@gmail.com>
2025-05-02 04:40:15 +0530

    Clean up compressor.py formatting and remove unnecessary whitespace

--- pyppd/compressor.py
+++ pyppd/compressor.py
@@ -2,19 +2,15 @@ from subprocess import Popen, PIPE
 
 def compress(value):
     """Compresses a byte array with the xz binary"""
-
     process = Popen(["xz", "--compress", "--force"], stdin=PIPE, stdout=PIPE)
     return process.communicate(value)[0]
 
 def decompress(value):
     """Decompresses a byte array with the xz binary"""
-
-    process = Popen(["xz", "--decompress", "--stdout", "--force"],
-                    stdin=PIPE, stdout=PIPE)
+    process = Popen(["xz", "--decompress", "--stdout", "--force"], stdin=PIPE, stdout=PIPE)
     return process.communicate(value)[0]
 
 def compress_file(path):
     """Compress the file at 'path' with the xz binary"""
-
     process = Popen(["xz", "--compress", "--force", "--stdout", path], stdout=PIPE)
     return process.communicate()[0]
